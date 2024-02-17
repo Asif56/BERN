00986cc9dda83c0ef7a862f44706411fe2536f4c
Asif Malik <asif56175@gmail.com>
2024-02-17 13:28:58 +0530

    update the package json

--- api/contract/contract.js
+++ api/contract/contract.js
@@ -1,4 +1,4 @@
-const {Web3}=require("Web3")
+const {Web3}=require("web3")
 const ABI=require("../ABI.json")
 const web3= new Web3('https://eth-sepolia.g.alchemy.com/v2/-5hNbXp19DrbGO8I7U5MLXwKp3uLnz5J')
 const contractAddress="0x3877663f86De524EA1b63dC22d99ea2D1F5bdfEa"
